9d3f3e066258336d4aa11d9ad4f89aebad63b537
Vishal.Karangale <Vishal.Karangale@ril.com>
2024-01-09 13:57:11 +0530

    experience data update

--- src/utils/experienceData.ts
+++ src/utils/experienceData.ts
@@ -8,6 +8,7 @@ export const ExperienceData = [
       "Working on Chatbot for farmers. goal is to create an LLM based chatbot that can assist agronomists to resolve farmers queries with the help provided data.",
       "We are using different version of Meta's LLAMA 2 model for this chatbot. i have built an API with LLAMA 2, retrival QA pipeline of langchain and django to include this chatbot in our webapp.",
       "Furthermore, I actively contribute to the Frontend team of Jio Cloud Gaming, a platform built with Next.js., i have mainly worked on fixing bugs on website.",
+      "Alog with web I have been working on a Intern mobile app with React Native"
     ],
     isCurrent: true,
     startDate: "6-12-2023",
